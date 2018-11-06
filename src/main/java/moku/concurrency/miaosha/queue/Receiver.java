4faeed155fc24e002877ecbeb7a28a0d9c2750ad
Louis <qingyanjiu@126.com>
2018-11-06 21:22:15 +0800

    added ack in receiver

--- src/main/java/moku/concurrency/miaosha/queue/Receiver.java
+++ src/main/java/moku/concurrency/miaosha/queue/Receiver.java
@@ -1,9 +1,13 @@
 package moku.concurrency.miaosha.queue;
 
+import com.rabbitmq.client.Channel;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
@@ -11,9 +15,11 @@ public class Receiver {
 
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Receiver.class);
 
-    @RabbitListener(queues="miaosha")
-    public void receiveMiaoshaMessage(Map params) {
-        logger.info("Receive:"+params);
+    @RabbitListener(queues = "miaosha")
+    public void receiveMiaoshaMessage(Map params, Message message, Channel channel) throws IOException {
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
+//        channel.basicNack(message.getMessageProperties().getDeliveryTag(), true, false);
+        logger.info("Receive:" + params);
     }
 
 }
--- src/main/java/moku/concurrency/miaosha/queue/Sender.java
+++ src/main/java/moku/concurrency/miaosha/queue/Sender.java
@@ -1,18 +1,22 @@
 package moku.concurrency.miaosha.queue;
 
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.UUID;
 
 @Component
 public class Sender {
 
     @Autowired
-    private AmqpTemplate template;
+    private RabbitTemplate template;
 
     public void sendMiaosha(Map params){
-        template.convertAndSend("miaosha",params);
+        CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
+        template.convertAndSend("miaosha", params, correlationData);
     }
 }
